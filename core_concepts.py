2c6ee0cc47f01d1432473499551c365b560e3f6b
shyam-goknit <shyam@goknit.com>
2026-07-31 19:33:21 +0530

    unversal way of initialization

--- core_concepts.py
+++ core_concepts.py
@@ -3,7 +3,7 @@ from langchain_core import output_parsers
 import importlib.metadata
 # pyrefly: ignore [missing-import]
 from dotenv import load_dotenv
-
+from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -33,11 +33,40 @@ def demo_batch_execution():
     ]
     response = chain.batch(inputs)
     print(response)
-    
+
+def demo_streaming():
+    prompt = ChatPromptTemplate.from_template("{question}")
+    model = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite", temperature=0.7)
+    output_parsers = StrOutputParser()
+    chain = prompt | model | output_parsers
+
+    for chunk in chain.stream({"question": "What is happened in Sansad Bhavan in India, in brief?"}):
+        print(chunk, end="", flush=True)
+    print("\n")
+
+def excercise_first_chain():
+    prompt = ChatPromptTemplate.from_template("Generate a market tagline for the product {product} for the target audience {audience}")
+    model = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite", temperature=0.7)
+    output_parser = StrOutputParser()
+    chain = prompt | model | output_parser
+
+    for chunk in chain.stream({"product": "Shampoo", "audience": "18-25 year old males"}):
+        print(chunk, end="", flush=True)
+    print("\n")
+
+def universal_way_of_initialization_of_model():
+    model = init_chat_model("gemini-3.5-flash-lite", temperature=0.7, model_provider="google_genai")
+    response = model.invoke("why the sky is blue in one line?")
+    print(response.content[0].get("text"))
+
 
 def main():
-    demo_basic_chain()
-    demo_batch_execution()
+    # demo_basic_chain()
+    # demo_batch_execution()
+    # demo_streaming()
+    # excercise_first_chain()
+    universal_way_of_initialization_of_model()
+
 
 if __name__ == "__main__":
     main()
